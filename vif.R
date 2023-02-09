793d3da0778c8dd2626c3c6d45b3c205fe455c10
Jeffrey Young <32875066+jyoungeconomics@users.noreply.github.com>
2023-02-09 15:15:22 -0600

    Update vif.R

--- vif.R
+++ vif.R
@@ -21,7 +21,7 @@ VIF <- function(moo){
 set.seed(123)
 y <- rnorm(mean = pi,sd = 3*pi/2,n = 100) #pure randomness
 x1 <- runif(min = -1,max = 1,n = 100)+(y/2)^2 #less pure randomness: transformed y plus noise
-x2 <- Rainfall[1:100]+x1*runif(min = 3,max = 5,n = 100) #annual rainfall in Nevada City, CA plus random noise
+x2 <- bootstrap::Rainfall[1:100]+x1*runif(min = 3,max = 5,n = 100) #annual rainfall in Nevada City, CA plus random noise
 x3 <- (runif(min = 3,max = 5,n = 100)+(100*x1)^2+sqrt(abs(y)))*0.001 #combining some of the random noise
 cor(x1,x2) #correlated?
 cor(x1,x3) #correlated?
